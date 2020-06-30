ac6e1ef7687f8b9be1d19f1665270e484d1535df
Akram <aslackw@riseup.net>
2020-06-30 14:33:31 +0100

    Writer: use QRandomGenerator instead of qrand() in QT >= 5.10 (#63)
    Co-authored-by: Akram Abdeslem Chaima <akram@riseup.net>

--- sources/writer.cpp
+++ sources/writer.cpp
@@ -7,6 +7,10 @@
 #include <QTextStream>
 #include <QCoreApplication>
 #include <QDebug>
+#include <QtGlobal>
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 10, 0))
+    #include <QRandomGenerator>
+#endif
 
 #include "include/qtcsv/abstractdata.h"
 #include "sources/filechecker.h"
@@ -180,7 +184,11 @@ QString WriterPrivate::getTempFileName()
 
     for (int counter = 0; counter < std::numeric_limits<int>::max(); ++counter)
     {
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 10, 0))
+        QString name = nameTemplate.arg(QString::number(QRandomGenerator::global()->generate()));
+#else
         QString name = nameTemplate.arg(QString::number(qrand()));
+#endif
         if ( false == QFile::exists(name) )
         {
             return name;
